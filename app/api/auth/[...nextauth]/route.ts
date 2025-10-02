aa37e0783055d680731ead3af4dace1d626c611c
tctc <1325109177@qq.com>
2025-10-02 16:15:01 +0800

    更新认证API的运行时配置，将运行时从"edge"更改为"nodejs"，以支持Postgres包。

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
 export const { GET, POST } = handlers
-export const runtime = "edge" // optional
+export const runtime = "nodejs" // 使用 Node.js 运行时以支持 postgres 包
